74fa515856aaaffd3a33f7c7ee663f9c3796b93e
Stefan Schöfegger <memo5@gmx.at>
2020-07-29 12:24:09 +0200

    Energy as float

--- mec.py
+++ mec.py
@@ -108,15 +108,15 @@ def mec_parse_data( data ) :
 		mec.set('/Ac/L3/Voltage', (data['VC']))
 		mec.set('/Ac/L3/Power', (data['PC']))
 
-		mec.set('/Ac/L1/Energy/Forward', (data['EFAA']/1000))
-		mec.set('/Ac/L1/Energy/Reverse', (data['ERAA']/1000))
-		mec.set('/Ac/L2/Energy/Forward', (data['EFAB']/1000))
-		mec.set('/Ac/L2/Energy/Reverse', (data['ERAB']/1000))
-		mec.set('/Ac/L3/Energy/Forward', (data['EFAC']/1000))
-		mec.set('/Ac/L3/Energy/Reverse', (data['ERAC']/1000))
+		mec.set('/Ac/L1/Energy/Forward', (float(data['EFAA'])/1000), 2)
+		mec.set('/Ac/L1/Energy/Reverse', (float(data['ERAA'])/1000), 2)
+		mec.set('/Ac/L2/Energy/Forward', (float(data['EFAB'])/1000), 2)
+		mec.set('/Ac/L2/Energy/Reverse', (float(data['ERAB'])/1000), 2)
+		mec.set('/Ac/L3/Energy/Forward', (float(data['EFAC'])/1000), 2)
+		mec.set('/Ac/L3/Energy/Reverse', (float(data['ERAC'])/1000), 2)
 
-		mec.set('/Ac/Energy/Forward', (data['EFAT']/1000))
-		mec.set('/Ac/Energy/Reverse', (data['ERAT']/1000))
+		mec.set('/Ac/Energy/Forward', (float(data['EFAT'])/1000), 2)
+		mec.set('/Ac/Energy/Reverse', (float(data['ERAT'])/1000), 2)
 
 		powertotal = data['PT']
 		print("++++++++++")
